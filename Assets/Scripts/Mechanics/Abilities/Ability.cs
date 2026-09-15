c3350eb0cf219d2520751b68232e76cce5b06c79
gianlucalucaa <gianlucagl17@gmail.com>
2026-09-15 02:31:29 -0400

    Creating Resources Folder

--- Assets/Scripts/Mechanics/Abilities/Ability.cs
+++ Assets/Scripts/Mechanics/Abilities/Ability.cs
@@ -1,9 +1,13 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
 
 public abstract class Ability : MonoBehaviour
 {
     public abstract int _triggerCost();
 
     public abstract void TriggerAbility();
+
+    [SerializeField]
+    public string Description;
 }
--- Assets/Scripts/UI/CharSelectScript.cs
+++ Assets/Scripts/UI/CharSelectScript.cs
@@ -5,7 +5,13 @@ public class CharSelectScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Character[] characterArray = Resources.LoadAll("ScriptableObjects/Character", typeOf(ScriptableObject)) 
+        as Character[];
+         foreach (var c in characterArray)
+        {
+            Debug.Log(c.GetCharacterType());
+        }
+
     }
 
     // Update is called once per frame
